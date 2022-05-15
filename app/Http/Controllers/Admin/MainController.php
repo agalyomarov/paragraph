7c3b84e3d6c012afb99586591f535422992537eb
Agaly Omarov <97389793+agalyomarov@users.noreply.github.com>
2022-05-15 19:36:55 +0300

    yes

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -8,23 +8,28 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         session(['isAdmin' => true]);
+        if ($request->query->has('search')) {
+            $search = $request->query->get('search');
+            $posts = Post::where('id', $search)->orWhere('url', 'LIKE', "%{$search}%")->orWhere('title', 'LIKE', "%{$search}%")->paginate(10);
+            $countPosts = count($posts);
+            return view('admin.index', compact('posts', 'countPosts', 'search'));
+        }
         $posts = Post::paginate(10);
         $countPosts = Post::all()->count();
-        // dd($posts);
         return view('admin.index', compact('posts', 'countPosts'));
     }
-    public function search(Request $request)
+    public function search($search, Request $request)
     {
+        dd($search);
         if (!$request->search) {
             return redirect()->route('admin.index');
         }
         session(['isAdmin' => true]);
-        $search = $request->search;
         $posts = Post::where('id', $search)->orWhere('url', 'LIKE', "%{$search}%")->orWhere('title', 'LIKE', "%{$search}%")->paginate(10);
-        $countPosts = Post::all()->count();
+        $countPosts = count($posts);
         return view('admin.index', compact('posts', 'countPosts', 'search'));
     }
     public function delete(Request $request)
